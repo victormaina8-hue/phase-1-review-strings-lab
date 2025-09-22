da5c5c1b4eccb1bd3683b7e601ea8f07647977e4
victor maina <victor.maina8@student.moringaschool.com>
2025-09-22 11:24:21 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 // Write your code in this file!
 const currentUser = 'Grace Hopper';
-const welcomeMessage = `currentUser`;
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
+
+
